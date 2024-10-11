d9864d7edfa6c9d84f032b3043a21090b020dc19
Artūrs Lastiņš <arturs.lastins@gmail.com>
2024-10-11 09:51:11 +0300

    removed primeVue and used dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css';
 import 'v-calendar/style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@vuepic/vue-datepicker/dist/main.css';
+import 'quill/dist/quill.snow.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
@@ -18,26 +19,6 @@ app.use(VCalendar, {});
 
 import VueDatePicker from '@vuepic/vue-datepicker';
 app.component('VueDatePicker', VueDatePicker);
-
-// PrimeVue
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import Editor from 'primevue/editor';
-import Slider from 'primevue/slider';
-import ColorPicker from 'primevue/colorpicker';
-
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
-        options: {
-            darkModeSelector: '.class-mate-dark',
-        },
-    },
-});
-
-app.component('Editor', Editor);
-app.component('Slider', Slider);
-app.component('ColorPicker', ColorPicker);
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
