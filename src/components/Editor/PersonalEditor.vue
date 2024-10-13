ed21e1a17e18119e81468b89a2a6cc9f75907b0f
LabaisLatvietis <44544653+LabaisLatvietis@users.noreply.github.com>
2024-10-13 11:34:06 +0300

    sanitizeUrl pārvietota uz helperi Quill pamatiestatījumu maiņa pārvietota uz main.ts

--- src/components/Editor/PersonalEditor.vue
+++ src/components/Editor/PersonalEditor.vue
@@ -58,16 +58,6 @@ export default {
                 ],
             },
         });
-
-        const Link = Quill.import('formats/link');
-        Link.sanitize = (url: string) => {
-            if (!url.startsWith('http')) {
-                return `https://${url}`;
-            }
-            return url;
-        };
-        Quill.register(Link, true);
-
         document.querySelectorAll('.ql-toolbar').forEach((item) => {
             item.classList.add('editor__wrapper__toolbar');
         });
